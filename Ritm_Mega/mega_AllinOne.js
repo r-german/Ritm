b4dfff9214ecf4029e2bb028ee227ef43a9936e7
r-german <111256133+r-german@users.noreply.github.com>
2024-02-14 06:07:20 +0300

    Add files via upload

---
+++ Ritm_Mega/mega_AllinOne.js
@@ -0,0 +1,6 @@
+//@prepros-append ../Ritm_src/js/gsap.min.js
+//@prepros-append ../Ritm_src/js/ScrollTrigger.min.js
+//@prepros-append ../Ritm_src/js/ScrollSmoother.min.js
+//@prepros-append ../Ritm_src/js/common.js
+//@prepros-append ../Ritm_src/js/swiper-bundle.min.js
+//@prepros-append mega.js
